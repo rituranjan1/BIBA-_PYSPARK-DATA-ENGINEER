457d79ba9babc5ba346318558330de0262b99334
Ritu Ranjan Kumar <95923495+rituranjan1@users.noreply.github.com>
2024-01-02 10:47:56 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local[1]") \
+                    .appName('SparkByExamples.com') \
+                    .getOrCreate()
+
+data = [("James","Smith","USA","CA"),("Michael","Rose","USA","NY"), \
+    ("Robert","Williams","USA","CA"),("Maria","Jones","USA","FL") \
+  ]
+columns=["firstname","lastname","country","state"]
+df=spark.createDataFrame(data=data,schema=columns)
+df.show()
